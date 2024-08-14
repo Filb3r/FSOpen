f0e2ae81431bed297e35aa2782cac1544c12dcf6
Filb3r <villedsn@gmail.com>
2024-08-14 18:17:26 +0300

    osa4

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
         }, '3000')
       })
       .catch(error => {
-        setErrorMessage(error.message)
+        setErrorMessage(error.response.data.error)
         setTimeout(() => {
           setErrorMessage(null)
         }, '3000')
