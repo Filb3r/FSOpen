e931e99610e2406907b56a543a3679cf818c27e9
F1lb3r <villedsn@gmail.com>
2024-10-16 18:17:16 +0300

    playwright testit wip

--- osa5/bloilista-testit/tests/blog_app.spec.js
+++ osa5/bloilista-testit/tests/blog_app.spec.js
@@ -15,6 +15,62 @@ describe('Blog app', () => {
   })
 
   test('Login form is shown', async ({ page }) => {
-    // ...
+    const locator = await page.getByText('Log in to application')
+
+    await expect(locator).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.goto('http://localhost:5173')
+
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.getByRole('textbox').last().fill('salainen')
+      await page.getByRole('button', { name: 'Login'}).click()
+
+      await expect(page.getByText('Matti Luukkainen logged in!')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.goto('http://localhost:5173')
+
+      await page.getByRole('textbox').first().fill('vaara')
+      await page.getByRole('textbox').last().fill('salaton')
+      await page.getByRole('button', { name: 'Login'}).click()
+
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong username or password!')
+    })
+  })
+})
+
+describe('When logged in', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('textbox').first().fill('mluukkai')
+    await page.getByRole('textbox').last().fill('salainen')
+    await page.getByRole('button', { name: 'Login'}).click()
+
+    await expect(page.getByRole('button', { name: 'Add'})).toBeVisible()
+  })
+
+  test('a new blog can be created', async ({ page }) => {
+    await page.getByRole('button', { name: 'Add'}).click()
+
+    await page.getByPlaceholder('Title').fill('TestiKirjaus')
+    await page.getByPlaceholder('Author').fill('T. Kirjanen')
+    await page.getByPlaceholder('URL').fill('www.testiurli.com')
+    await page.getByRole('button', { name: 'create'}).click()
+
+    const confirmationDiv = await page.locator('.verify')
+    await expect(confirmationDiv).toContainText('New blog created: TestiKirjaus')
+  })
+
+  test('a blog can be liked', async({page}) => {
+    await page.getByRole('button', {name: 'Show'}).click()
+
+    await page.getByRole('button', {name: 'like'}).click()
+
+  }) 
 })
