9dc78e8b0754d32e86e81b074800550bcf6b5a59
Filb3r <villedsn@gmail.com>
2024-10-28 20:56:17 +0200

    osa6 tehtävät wip

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,13 +1,20 @@
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { useQueryClient, useMutation, useQuery } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+  const updateAnecdoteMutation = useMutation({
+    mutationFn:updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const result = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    queryFn: getAnecdotes,
     retry: false
   })
   console.log(JSON.parse(JSON.stringify(result)))
@@ -22,9 +29,12 @@ const App = () => {
   
   const anecdotes = result.data
 
+
   const handleVote = (anecdote) => {
     console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
+  
 
   return (
     <div>
